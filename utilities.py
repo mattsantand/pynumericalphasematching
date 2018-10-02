2fe4137b48f39d87f0a6e83e449ad3babd8592c4
Matteo Santandrea <mattsantand@gmail.com>
2018-10-02 14:08:10 +0200

    updated the "calculate poling period" function in "utilities" to work in standard units (meter)

--- utilities.py
+++ utilities.py
@@ -1,7 +1,21 @@
 import numpy as np
 
 
-def calculate_poling_period(lamr, lamg, lamb, nr, ng, nb, order=1, **kwargs):
+def calculate_poling_period(lamr=0, lamg=0, lamb=0, nr=None, ng=None, nb=None, order=1, **kwargs):
+    """
+    Function to calculate the poling period of a specific process. To ensure energy conservation, specify only 2
+    wavelengths (in meter) and leave the free one to 0
+
+    :param lamr:
+    :param lamg:
+    :param lamb:
+    :param nr:
+    :param ng:
+    :param nb:
+    :param order:
+    :param kwargs:
+    :return:
+    """
     propagation_type = kwargs.get("propagation_type", "copropagation")
     if (lamb == 0):
         lamb = 1. / (1. / abs(lamg) + 1. / abs(lamr))
@@ -10,15 +24,15 @@ def calculate_poling_period(lamr, lamg, lamb, nr, ng, nb, order=1, **kwargs):
     if (lamr == 0):
         lamr = 1. / (1. / abs(lamb) - 1. / abs(lamg))
     if propagation_type.lower() == "copropagation":
-        Lambda = order / (nb(abs(lamb)) / lamb - ng(abs(lamg)) / lamg - nr(abs(lamr)) / lamr)
+        Lambda = order / (nb(abs(lamb) * 1e6) / lamb - ng(abs(lamg) * 1e6) / lamg - nr(abs(lamr) * 1e6) / lamr)
     elif propagation_type.lower() == "counterpropagation":
-        Lambda = order / (nb(abs(lamb)) / lamb - ng(abs(lamg)) / lamg + nr(abs(lamr)) / lamr)
+        Lambda = order / (nb(abs(lamb) * 1e6) / lamb - ng(abs(lamg) * 1e6) / lamg + nr(abs(lamr) * 1e6) / lamr)
     else:
         raise ValueError("Don't know " + propagation_type)
     return lamr, lamg, lamb, Lambda
 
 
-def deltabeta(lam1, lam2, free_param, nr, ng, nb, poling = np.infty):
+def deltabeta(lam1, lam2, free_param, nr, ng, nb, poling=np.infty):
     if free_param == "b":
         wlr, wlg = lam1, lam2
         wlb = (wlr ** -1 + wlg ** -1) ** -1
