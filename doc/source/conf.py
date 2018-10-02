9e0667b07aec384a55c6de4916c3157767050a9e
Matteo Santandrea <mattsantand@gmail.com>
2018-10-02 18:33:00 +0200

    Updated config.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,6 +40,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx']
 
 intersphinx_mapping =  {'numpy': ('http://docs.scipy.org/doc/numpy/', None)}
+autodoc_member_order = "bysource"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
