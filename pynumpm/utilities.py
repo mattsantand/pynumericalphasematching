e234c6bc8aa31b38ee34b42085c0d41070dae3cd
Matteo Santandrea <mattsantand@gmail.com>
2019-07-16 01:19:57 +0200

    Implemented function calculating the deltabeta. The poling period can be inferred from this function. Implemented function to calculate the phasematching point given a wavelength and a poling period.

--- pynumpm/utilities.py
+++ pynumpm/utilities.py
@@ -1,70 +1,107 @@
 import numpy as np
 from scipy.interpolate import UnivariateSpline
 from scipy.signal import savgol_filter
+import scipy.optimize as opt
 import logging
 import enum
 
+
 class Propagation(enum.Enum):
-    COPROPAGATION = "Copropagation"
-    COUNTEPROPAGATION = "Counterpropagation"
+    COPROPAGATION = -1
+    """Sign of the signal in copropagatio"""
+    COUNTEPROPAGATION = +1
+    """Sign of signal in counterpropagation"""
 
-def calculate_poling_period(lamr=0, lamg=0, lamb=0, nr=None, ng=None, nb=None, order=1,
-                            propagation=Propagation.COPROPAGATION, fulloutput = False):
+
+def deltabeta(lamr=None, lamg=None, lamb=None, nr=None, ng=None, nb=None, poling=np.infty, order=1,
+              propagation=Propagation.COPROPAGATION):
     """
-    Function to calculate the poling period of a specific process. To ensure energy conservation, specify only 2
-    wavelengths (in meter) and leave the free one to 0
-
+    Function to calculate the delta
     :param lamr:
     :param lamg:
     :param lamb:
     :param nr:
     :param ng:
     :param nb:
-    :param order:
-    :param kwargs:
-    :return:
-    """
-
-    if (lamb == 0):
-        lamb = 1. / (1. / abs(lamg) + 1. / abs(lamr))
-    if (lamg == 0):
-        lamg = 1. / (1. / abs(lamb) - 1. / abs(lamr))
-    if (lamr == 0):
-        lamr = 1. / (1. / abs(lamb) - 1. / abs(lamg))
-    if propagation == Propagation.COPROPAGATION:
-        Lambda = order / (nb(abs(lamb) * 1e6) / lamb - ng(abs(lamg) * 1e6) / lamg - nr(abs(lamr) * 1e6) / lamr)
-    elif propagation == Propagation.COUNTEPROPAGATION:
-        Lambda = order / (nb(abs(lamb) * 1e6) / lamb - ng(abs(lamg) * 1e6) / lamg + nr(abs(lamr) * 1e6) / lamr)
-    else:
-        raise ValueError("Don't know {0}".format(propagation))
-    if fulloutput:
-        return lamr, lamg, lamb, Lambda
-    return Lambda
-
-def deltabeta(lam1, lam2, free_param, nr, ng, nb, poling=np.infty):
-    """
-    Function to calculate the delta
-    :param lam1:
-    :param lam2:
-    :param free_param:
-    :param nr:
-    :param ng:
-    :param nb:
     :param poling:
+    :param order:
     :return:
     """
-    if free_param == "b":
-        wlr, wlg = lam1, lam2
-        wlb = (wlr ** -1 + wlg ** -1) ** -1
-    elif free_param == "g":
-        wlr, wlb = lam1, lam2
-        wlg = (wlb ** -1 - wlr ** -1) ** -1
-    elif free_param == "r":
-        wlg, wlb = lam1, lam2
-        wlr = (wlb ** -1 - wlg ** -1) ** -1
+    importError = True
+    if lamr is None and lamg is not None and lamb is not None:
+        importError = False
+        lamr = (lamb ** -1 - lamg ** -1) ** -1
+    if lamg is None and lamr is not None and lamb is not None:
+        importError = False
+        lamg = (lamb ** -1 - lamr ** -1) ** -1
+    if lamb is None and lamr is not None and lamg is not None:
+        importError = False
+        lamb = (lamr ** -1 + lamg ** -1) ** -1
+
+    if importError:
+        raise ValueError("Only one of the input wavelengths can be 'None'")
+
+    return 2 * np.pi * (nb(lamb * 1e6) / lamb - ng(lamg * 1e6) / lamg +
+                        propagation.value * nr(lamr * 1e6) / lamr - order / poling)
+
+
+def calculate_phasematching_point(fixed_wl, poling_period, nb, ng, nr, hint, order=1, verbose=False):
+    lam, constlam = fixed_wl
+    lam *= 1e6
+    poling_period_um = poling_period * 1e6
+    hint = [i * 1e6 for i in hint]
+
+    def zb(w):
+        wb = lam
+        wg, wr = w
+
+        return np.array([nb(abs(wb)) / wb - ng(abs(wg)) / wg - nr(abs(wr)) / wr - 1.0 * order / poling_period_um,
+                         1.0 / (1.0 / abs(wb) - 1.0 / abs(wg)) - abs(wr)])
+
+    def zg(w):
+        wg = lam
+        wb, wr = w
+        return np.array([nb(abs(wb)) / wb - ng(abs(wg)) / wg - nr(abs(wr)) / wr - 1.0 * order / poling_period_um,
+                         1.0 / (1.0 / abs(wb) - 1.0 / abs(wg)) - abs(wr)])
+
+    def zr(w):
+        wr = lam
+        wb, wg = w
+        return np.array([nb(abs(wb)) / wb - ng(abs(wg)) / wg - nr(abs(wr)) / wr - 1.0 * order / poling_period_um,
+                         1.0 / (1.0 / abs(wb) - 1.0 / abs(wg)) - abs(wr)])
+
+    def zshg(w):
+        wshg, wfund = w
+        return np.array([nb(abs(wshg)) / wshg - ng(abs(wfund)) / wfund - nr(
+            abs(wfund)) / wfund - order / poling_period_um, abs(wfund) - 2. * abs(wshg)])
+
+    if constlam == 'shg':
+        out = opt.fsolve(zshg, hint, full_output=True)
+        if (out[2] == 1):
+            # arr = np.sort(np.array([out[0][0], out[0][1], out[0][1]]))
+            arr = np.array([out[0][0], out[0][1], out[0][1]])  # edited by Matteo on 24.10.2017
+            return True, np.array([arr[0], arr[1], arr[2], poling_period_um]) * 1e-6
+        else:
+            if verbose:
+                print("Error?:\n", out)
+            return False, np.array([np.nan, np.nan, np.nan, np.nan])
+
     else:
-        raise ValueError("Wrong label for free parameter")
-    return wlr, wlg, wlb, 2 * np.pi * (nb(wlb) / wlb - ng(wlg) / wlg - nr(wlr) / wlr - 1 / poling)
+        if constlam == 'b':
+            out = opt.fsolve(zb, hint, full_output=True)
+        if constlam == 'g':
+            out = opt.fsolve(zg, hint, full_output=True)
+        if constlam == 'r':
+            out = opt.fsolve(zr, hint, full_output=True)
+
+        if (out[2] == 1):
+            # arr = np.sort(np.array([lam, out[0][0], out[0][1]]))
+            arr = np.array([lam, out[0][0], out[0][1]])
+            return True, np.array([arr[0], arr[1], arr[2], poling_period_um]) * 1e-6
+        else:
+            if verbose:
+                print("Error?:\n", out)
+            return False, np.array([np.nan, np.nan, np.nan, np.nan])
 
 
 def bandwidth(wl, phi, **kwargs):
@@ -124,4 +161,3 @@ def calculate_profile_properties(z=None, profile=None):
     power_spectrum = np.fft.fftshift(power_spectrum)
     z_autocorr = np.fft.fftshift(np.fft.fftfreq(len(f), np.diff(f)[0]))
     return z_autocorr, autocorrelation, f, power_spectrum
-
